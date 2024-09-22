759a87a39bf6aa5b83b70e39d0bb2a30ea88b416
rywk <rywkbones@gmail.com>
2024-09-22 18:52:30 -0300

    test

--- build_web_game.sh
+++ build_web_game.sh
@@ -1,2 +1,2 @@
-go build -o ./cmd/web-server/miniao.exe ./cmd/run-client
+GOOS=windows GOARCH=amd64 go build -o ./cmd/web-server/miniao.exe ./cmd/run-client
 GOOS=js GOARCH=wasm go build -o ./cmd/web-server/main.wasm ./cmd/run-web-client
