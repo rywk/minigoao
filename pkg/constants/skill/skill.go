68a7a836d1a15dbdc8b8280a37d0348e58f82490
rywk <rywkbones@gmail.com>
2024-10-05 05:16:15 -0300

    a

--- pkg/constants/skill/skill.go
+++ pkg/constants/skill/skill.go
@@ -121,7 +121,7 @@ func (s Skills) Buffs() Buffs {
 	b[BuffPhysicalDamage] -= s[Intelligence] * 0.4
 
 	b[BuffMagicDamage] += s[Intelligence]
-	b[BuffMagicDamage] -= s[Vitality]
+	//b[BuffMagicDamage] -= s[Vitality]
 
 	return b
 }
